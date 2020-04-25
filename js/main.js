31078e17403960f54a86a07e7aadf36e155eae5b
SophiaNM <njerimurage92@outlook.com>
2020-04-25 12:54:16 +0200

    Adding zoom and implementing beneficiary Type switch

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ var barTooltip = d3.select("body")
 	.attr("class", "tooltip");
 
 var positionInfo = document.getElementById('mapDiv').getBoundingClientRect();
-var margin = {top: 45, right: 10, bottom: 20, left: -80};
+var margin = {top: 45, right: 10, bottom: 20, left: -70};
 var width = positionInfo.width;
 	width = width - margin.left - margin.right;
 var height = positionInfo.height + margin.bottom;
@@ -23,6 +23,8 @@ var main = mapDiv.append("svg")
     // .attr("height", height);
 var svg = main.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var mapG = svg.append("g")
 
 // Map and projection
 var path = d3.geoPath();
@@ -52,12 +54,17 @@ function highlightCountry(d){
 	if(d3.select(this).classed('mapBorder') === false){
 		d3.select(this).classed("mapBorderHigh", true);
 	}
-	barTooltip.html('<b>' +idToName(d.id) + '</b><br />');
+	barTooltip.html('<b>' +idToName(d.id) + '</b> <br/>');
 	barTooltip.transition()
 		.duration(50)
 		.style("display", "block")
 		.style("opacity", 1);
-	
+}
+
+function highlightChoropleth(d) {
+
+	return console.log("YYaya")
+
 }
 
 function onMouseMove(){
@@ -92,16 +99,19 @@ function onMapClick(d){
 		.style("fill-opacity", 1);
 }
 
-function onFlowLegendClick(){
+function onFlowLegend(d){
+	// if (d === "Fleet to Processing") {
+	// 	if (d3.selectAll("#process-market") === True)
+	// 		d3.selectAll("#arcs")
+	// 			.attr("stroke-opacity", 0.1)
+	// 			.attr("fill-opacity", 0.1);
+	// 		// .attr("stroke", "blue");
+	// }
+	// else if ( d === "Resource to Processing"){}
+	// else {}
 	return console.log("yay")
-	d3.selectAll(".bar_chart")
-		.style("stroke-opacity", 0.2)
-		.style("fill-opacity", 0.2);
-
-	d3.selectAll("#bar_" + d.id)
-		.style("stroke-opacity", 1)
-		.style("fill-opacity", 1);
 }
+
 
 // Load Data
 var promises = [
@@ -109,12 +119,13 @@ var promises = [
 	d3.json("data/flows.json"),
 	d3.json("data/stackDataPrepared.json"),
 	d3.json("data/sankey_data.json"),
+	d3.json("data/worldcountriesChoropleth.json"),
 	d3.csv("data/beneficiary.csv")
 
 ];
 Promise.all(promises).then(ready);
 
-function ready([topo, flows, stack, graph, choropleth]) {
+function ready([topo, flows, stack, graph, topoChoropleth, beneficiaryData]) {
 	continentFlows = {
 		"nodes": [],
 		"links": []
@@ -159,44 +170,31 @@ function ready([topo, flows, stack, graph, choropleth]) {
 
 
 
-	// Draw the map
-	// var mapG = svg.append("g")
-	// 	// .attr("transform", "translate(-50,20)")
-	// 	.selectAll("path")
-	// 	.data(topojson.feature(topo, topo.objects.countries).features)
-	// 	.enter()
-	// 	.append("path")
-	// 	.attr("id", function(d){return "path_" + d.id})
-	// 	.attr("class", "mapBackground")
-	// 	// draw each country
-	// 	.attr("d", d3.geoPath()
-	// 	   .projection(projection)
-	// 	)
-	// 	.on("click", onMapClick)
-	// 	.on("mousemove", onMouseMove)
-	// 	.on('mouseover', highlightCountry)
-	// 	.on('mouseout', clearHighlight);
-
 	var zoom = d3.zoom()
 		.scaleExtent([1, 8])
-		.on("zoom", zoomed);
+		.on("zoom", zoomed)
+		;
 
+	// zoom.translate([100,100])
 	// mapG.call(zoom);
 
 	function zoomed() {
 		var transform = d3.event.transform;
-		svg.style("stroke-width", 1.9 / transform.k + "px");
-		svg.attr("transform", transform);
+		mapG.style("stroke-width", 1.9 / transform.k + "px");
+		mapG.attr("transform", transform);
+		// svg.selectAll(".exclude-from-zoom").attr("transform", "scale(" + d3.event.scale + ")");
 	}
 
 	d3.select('#zoom-in').on('click', function() {
 		// Smooth zooming
-		zoom.scaleBy(svg.transition().duration(750), 1.3);
+		zoom.scaleBy(mapG.transition().duration(750), 1.3);
+		// d3.selectAll(".exclude-from-zoom").attr("transform", "scale(" + d3.event.scale + ")");
 	});
 
 	d3.select('#zoom-out').on('click', function() {
 		// Ordinal zooming
-		zoom.scaleBy(svg.transition().duration(750), 1 / 1.3);
+		zoom.scaleBy(mapG.transition().duration(750), 1 / 1.3);
+		// d3.selectAll(".exclude-from-zoom").attr("transform", "scale(" + d3.event.scale + ")");
 	});
 
 
@@ -221,10 +219,16 @@ function ready([topo, flows, stack, graph, choropleth]) {
 		});
 
 	function renderFlow() {
-		// Draw the map
-		var mapG = svg.append("g")
-			// .attr("transform", "translate(-50,20)")
-			.selectAll("path")
+		// Clear all previous drawn elements
+		d3.selectAll(".choroplethBackground").remove()
+		d3.selectAll("#choroplethLegend").remove()
+		d3.selectAll("#arcs").remove()
+		d3.selectAll("#circles").remove()
+		d3.selectAll(".mapBackground").remove()
+		d3.selectAll("#mapLegend").remove()
+
+		// Draw new elements
+		var map = mapG.selectAll("path")
 			.data(topojson.feature(topo, topo.objects.countries).features)
 			.enter()
 			.append("path")
@@ -239,171 +243,243 @@ function ready([topo, flows, stack, graph, choropleth]) {
 			.on('mouseover', highlightCountry)
 			.on('mouseout', clearHighlight);
 
-	var center = svg.append("g")
-		.selectAll("circle")
-		.data(flows).enter().append("g");
-	center.append("circle")
-		.attr("class", "control")
-		.attr("r", 7)
-		.attr("fill", "none")
-		.attr("stroke", "darkblue")
-		.attr("stroke-width", 2)
-		.attr("cx", function(d){
-			return projection(d.country.origin)[0];
-		})
-		.attr("cy", function(d){
-			return projection(d.country.origin)[1];
+		//Add end and Start Node Circles
+		var center = mapG.append("g")
+			.selectAll("circle")
+			.data(flows).enter().append("g")
+			.attr("id", "circles");;
+		center.append("circle")
+			.attr("class", "control")
+			.attr("r", 7)
+			.attr("fill", "none")
+			.attr("stroke", "darkblue")
+			.attr("stroke-width", 2)
+			.attr("cx", function(d){
+				return projection(d.country.origin)[0];
+			})
+			.attr("cy", function(d){
+				return projection(d.country.origin)[1];
+			});
+		center.append("circle")
+			.attr("class", "control")
+			.attr("r", 7)
+			.attr("fill", "none")
+			.attr("stroke", "green")
+			.attr("stroke-width", 2)
+			.attr("cx", function(d){
+				return projection(d.country.destination)[0];
+			})
+			.attr("cy", function(d){
+				return projection(d.country.destination)[1];
+			});
+
+		// Add flows
+		line = d3.line();
+		var arcs = mapG.append("g").attr("id", "arcs");
+		arcNodes = arcs.selectAll("path.lines")
+			.data(flows).enter()
+			.append("path")
+			.attr("class", "lines linesMovement")
+			.attr("stroke-width", function(d) {
+				return 0.07 * d.value;
+			})
+			/*.attr('marker-end', function(d){
+				if((d.country.beneficiary_typefrom == "p" && d.country.beneficiary_typeto == "m") | ((d.country.beneficiary_typefrom == "m" && d.country.beneficiary_typeto == "p"))) return "url(#arrow-pm-mp)";
+				else if((d.country.beneficiary_typefrom == "f" && d.country.beneficiary_typeto == "p") | ((d.country.beneficiary_typefrom == "p" && d.country.beneficiary_typeto == "f"))) return "url(#arrow-fp-pf)";
+				else return 'url(#arrow-fr)';
+			})*/
+			.attr('stroke-dasharray', '15, 5, 5, 10')
+			.attr("stroke-linecap", "round")
+			.attr("stroke-linejoin", "round")
+			.attr("id", function(d){
+				if((d.country.beneficiary_typefrom == "p" && d.country.beneficiary_typeto == "m") | ((d.country.beneficiary_typefrom == "m" && d.country.beneficiary_typeto == "p"))) return "process-market";
+				else if((d.country.beneficiary_typefrom == "f" && d.country.beneficiary_typeto == "p") | ((d.country.beneficiary_typefrom == "p" && d.country.beneficiary_typeto == "f"))) return "fleet-process";
+				else return "resource-process";
+			})
+			.attr("stroke", function(d){
+				if((d.country.beneficiary_typefrom == "p" && d.country.beneficiary_typeto == "m") | ((d.country.beneficiary_typefrom == "m" && d.country.beneficiary_typeto == "p"))) return "#00bcd4";
+				else if((d.country.beneficiary_typefrom == "f" && d.country.beneficiary_typeto == "p") | ((d.country.beneficiary_typefrom == "p" && d.country.beneficiary_typeto == "f"))) return "#a3a0fb";
+				else return "#f00314";
+			})
+			.attr('d', function(d) {
+				return createArcs(line([projection(d.country.origin), projection(d.country.destination)]));
+			});
+		arcNodes.append("svg:title").text(function(d) {
+			beneficiarytypefrom = d.country.beneficiary_typefrom
+			beneficiarytypeto = d.country.beneficiary_typeto
+
+			if ( beneficiarytypefrom == 'p'){beneficiarytypefrom = 'processing'}
+			else if ( beneficiarytypefrom == 'm') {beneficiarytypefrom = 'market'}
+			else if ( beneficiarytypefrom == 'f') {beneficiarytypefrom = 'fleet'}
+			else {beneficiarytypefrom = 'fleet and resource'};
+
+			if ( beneficiarytypeto == 'p'){beneficiarytypeto = 'processing'}
+			else if ( beneficiarytypeto == 'm') {beneficiarytypeto = 'market'}
+			else if ( beneficiarytypeto == 'f') {beneficiarytypeto = 'fleet'}
+			else {beneficiarytypeto = 'fleet and resource'};
+
+
+			return d.country.fromName+" -> "+d.country.toName+"\n" + d.value +"% share" + "\n" + beneficiarytypefrom + " -> " + beneficiarytypeto;
 		});
-	center.append("circle")
-		.attr("class", "control")
-		.attr("r", 7)
-		.attr("fill", "none")
-		.attr("stroke", "green")
-		.attr("stroke-width", 2)
-		.attr("cx", function(d){
-			return projection(d.country.destination)[0];
-		})
-		.attr("cy", function(d){
-			return projection(d.country.destination)[1];
-		});
 
 
-	line = d3.line();
-	var arcs = svg.append("g").attr("id", "arcs");	
-	arcNodes = arcs.selectAll("path.lines")
-		.data(flows).enter()
-		.append("path")	
-		.attr("class", "lines linesMovement")
-		.attr("stroke-width", function(d) { 
-			return 0.07 * d.value;
-		})
-		/*.attr('marker-end', function(d){
-			if((d.country.beneficiary_typefrom == "p" && d.country.beneficiary_typeto == "m") | ((d.country.beneficiary_typefrom == "m" && d.country.beneficiary_typeto == "p"))) return "url(#arrow-pm-mp)"; 
-			else if((d.country.beneficiary_typefrom == "f" && d.country.beneficiary_typeto == "p") | ((d.country.beneficiary_typefrom == "p" && d.country.beneficiary_typeto == "f"))) return "url(#arrow-fp-pf)"; 
-			else return 'url(#arrow-fr)';
-		})*/
-		.attr('stroke-dasharray', '15, 5, 5, 10')
-		.attr("stroke-linecap", "round")
-		.attr("stroke-linejoin", "round")
-		.attr("stroke", function(d){
-			if((d.country.beneficiary_typefrom == "p" && d.country.beneficiary_typeto == "m") | ((d.country.beneficiary_typefrom == "m" && d.country.beneficiary_typeto == "p"))) return "#00bcd4";
-			else if((d.country.beneficiary_typefrom == "f" && d.country.beneficiary_typeto == "p") | ((d.country.beneficiary_typefrom == "p" && d.country.beneficiary_typeto == "f"))) return "#a3a0fb";
-			else return "#f00314";
-		})
-		.attr('d', function(d) {
-			return createArcs(line([projection(d.country.origin), projection(d.country.destination)]));
-		});
-	arcNodes.append("svg:title").text(function(d) {
-		beneficiarytypefrom = d.country.beneficiary_typefrom
-		beneficiarytypeto = d.country.beneficiary_typeto
+		// Defining flow map legend
+		mapkeys = ["Resource to Processing","Fleet to Processing", "Processing to Market"]
+		var colorScale = d3.scaleOrdinal()
+			.range(["#00bcd4", "#a3a0fb", "#f00314"])
+			.domain(mapkeys);
 
-		if ( beneficiarytypefrom == 'p'){beneficiarytypefrom = 'processing'}
-		else if ( beneficiarytypefrom == 'm') {beneficiarytypefrom = 'market'}
-		else if ( beneficiarytypefrom == 'f') {beneficiarytypefrom = 'fleet'}
-		else {beneficiarytypefrom = 'fleet and resource'};
+		var Leg = svg.append("g").attr("id","mapLegend")
+			.attr("class", "exclude-from-zoom");
 
-		if ( beneficiarytypeto == 'p'){beneficiarytypeto = 'processing'}
-		else if ( beneficiarytypeto == 'm') {beneficiarytypeto = 'market'}
-		else if ( beneficiarytypeto == 'f') {beneficiarytypeto = 'fleet'}
-		else {beneficiarytypeto = 'fleet and resource'};
+		var mapFlowlegend = Leg.selectAll(".legend")
+			.data(mapkeys.slice())
+			.enter().append("g")
+			.attr("class", "legend")
+			.on("click", onFlowLegend)
+			.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
+		var mapL = mapFlowlegend.append('g')
+			.attr("transform", "translate(1000,260)");
 
-		return d.country.fromName+" -> "+d.country.toName+"\n" + d.value +"% share" + "\n" + beneficiarytypefrom + " -> " + beneficiarytypeto;
-	});
+			mapL.append("rect")
+				.attr("x", -18)
+				.attr("width", 15)
+				.attr("height", 15)
+				.style("fill", function(d){return colorScale(d)});
 
-
-	mapkeys = ["Resource to Processing","Fleet to Processing", "Processing to Market"]
-	var colorScale = d3.scaleOrdinal()
-		.range(["#00bcd4", "#a3a0fb", "#f00314"])
-		.domain(mapkeys);
-
-	var mapFlowlegend = svg.selectAll(".legend")
-		.data(mapkeys.slice())
-		.enter().append("g")
-		.attr("class", "legend")
-		.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-	var mapL = mapFlowlegend.append('g')
-		.attr("transform", "translate(1000,260)");
-
-		mapL.append("rect")
-			.attr("x", -18)
-			.attr("width", 15)
-			.attr("height", 15)
-			.style("fill", function(d){return colorScale(d)});
-
-		mapL.append("text")
-			.attr("x", -24)
-			.attr("y", 9)
-			.attr("dy", ".35em")
-			.style("text-anchor", "end")
-			.text(function(d) {return d});
-			// .on("click", function(d){
-			// 	d3.selectAll("arcNodes")
-			// 		.style("stroke-opacity", 0.1)
-			// 		.style("fill-opacity", 0.1);
-			//
-			// 	return console.log("yay")} );
+			mapL.append("text")
+				.attr("x", -24)
+				.attr("y", 9)
+				.attr("dy", ".35em")
+				.style("text-anchor", "end")
+				.text(function(d) {return d});
 
 	};
 
-	function renderResource() {
 
-		console.log(choropleth[0].id)
-		const choroplethData = choropleth.reduce((accumulator, d) =>{
-			accumulator[d.perc_processing] = d;
-			return accumulator;
-		}, {});
 
-		const countries = topojson.feature(topo, topo.objects.countries);
-		countries.features.forEach(d => {
-			Object.assign(d.properties, choroplethData[d.id]);
-		})
-		// var colorScheme = d3.schemeReds[6];
-		// colorScheme.unshift("#eee")
-		// var colorScale = d3.scaleThreshold()
-		// 	.domain([1, 3, 6, 9, 12, 15])
-		// 	.range(colorScheme);
 
-		// choropl = d3.csv("data/beneficiary.csv",function(d) { data.set(d.code, +d.perc_processing)});
 
-		console.log(countries.features)
 
-		var mapG = svg.append("g")
+	function renderResource(beneficiaryType) {
+		// Defining ColorScale
+		d3.selectAll("#arcs").remove()
+		d3.selectAll("#circles").remove()
+		d3.selectAll(".mapBackground").remove()
+		d3.selectAll("#mapLegend").remove()
+		d3.selectAll("#choroplethLegend").remove()
+		d3.selectAll(".choroplethBackground").remove()
+
+		// Choropleth Data Merge
+		var countries = topojson.feature(topo, topo.objects.countries);
+		var countryName = beneficiaryData.reduce((accumulator, d) =>{accumulator[d.id] = d;return accumulator;}, {});
+		countries.features.forEach(d => {	Object.assign(d.properties, countryName[d.id]);});
+
+		// Choropleth Color Scheme
+		var colorValue = d => d.properties.perc_resource;
+		var colorScheme = d3.schemeBlues[6];
+			colorScheme.unshift("#ecf0ff")
+		var colorScale
+
+		if (beneficiaryType === "resource"){
+			colorValue = d => d.properties.perc_resource;
+			colorScale = d3.scaleThreshold()
+				.domain([0.01, 1, 4, 10, 15, 20])
+				.range(colorScheme);
+		}
+		else if (beneficiaryType === "fleet") {
+			colorValue = d => d.properties.perc_fleet;
+			colorScale = d3.scaleThreshold()
+				.domain([1, 3, 6, 9, 12, 15])
+				.range(colorScheme);
+		}
+		else if (beneficiaryType === "processing") {
+			colorScale = d3.scaleThreshold()
+				.domain([1, 3, 6, 8, 10, 12])
+				.range(colorScheme);
+			colorValue = d => d.properties.perc_processing;
+		}
+
+		//Loading choropleth  map
+		var mapChoropleth = mapG.append("g")
+		mapChoropleth.attr("class", "choroplethBackground")
 			.selectAll("path")
-			.data(countries.features )
+			.data(countries.features)
 			.enter()
 			.append("path")
-			.attr("id", function(d){return "path_" + d.id})
-			// .attr("class", "mapBackground")
-			// draw each country
 			.attr("d", d3.geoPath()
-			   .projection(projection))
-			.attr("fill", "red")
+				.projection(projection)	)
+			.attr("fill", d => (colorValue(d) === undefined )
+				? '#ecf0ff'
+				: colorScale(colorValue(d)))
+			.attr("stroke", 2)
+			.append('title')
+			.text(`${d => d}%Share`)
+			.exit().remove()
 			.on("click", onMapClick)
 			.on("mousemove", onMouseMove)
-			.on('mouseover', highlightCountry)
-			.on('mouseout', clearHighlight)
-			.text(d => d.properties);
+			// .on('mouseover', highlightCountry)
+			.on('mouseout', clearHighlight);
 
-		// 	// .attr("d", path);
-		// mapG.selectAll("path")
-		// 	.data(choropleth, function(d) { data.set(d.code, +d.perc_processing); })
-		// 	.enter()
-		// 	.append("path")
-		// 	.attr("fill", function (d){
-		// 		// Pull data for this country
-		// 		d.perc_processing = data.get(d.id) || 0;
-		// 		// Set the color
-		// 		return colorScale(d.perc_processing)});
+		// Choropleth Legend
+		var x = d3.scaleSqrt()
+			.domain([0.1,32])
+			.rangeRound([0,200])
 
+		g = svg.append("g")
+			.attr("id","choroplethLegend")
+			.attr("class", "key")
+			.attr("transform", "translate(820,320)");
 
+		g.selectAll("rect")
+			.data(colorScale.range().map(function(d) {
+				d = colorScale.invertExtent(d);
+				if (d[0] == null) d[0] = x.domain()[0];
+				if (d[1] == null) d[1] = x.domain()[1];
+				return d;
+			}))
+			.enter().append("rect")
+			.attr("height", 8)
+			.attr("x", function(d) { return x(d[0]); })
+			.attr("width", function(d) { return x(d[1]) - x(d[0]); })
+			.attr("fill", function(d) { return colorScale(d[0]); });
+
+		g.append("text")
+			.attr("class", "caption")
+			.attr("x", x.range()[0])
+			.attr("y", -9)
+			.attr("fill", "#000")
+			.attr("text-anchor", "start")
+			.attr("font-weight", "bold")
+			.text("Resource Percentage Distribution");
+
+		g.call(d3.axisBottom(x)
+			.tickSize(13)
+			.tickValues(colorScale.domain()))
+			.select(".domain")
+			.remove();
 
 
 	};
 
-	// renderResource()
+	// Switch map views
 	renderFlow()
+	d3.select('#overview').on('click', function(){
+		renderFlow()
+	})
+	d3.select('#resource').on('click', function(){
+		renderResource("resource")
+	})
+	d3.select('#fleet').on('click', function(){
+		renderResource("fleet")
+	})
+	d3.select('#process').on('click', function(){
+		renderResource("processing")
+	})
+
+
+
 
 	//Bar Chart
 	
@@ -464,6 +540,8 @@ function ready([topo, flows, stack, graph, choropleth]) {
 		.attr("dy", ".35em")
 		.attr("transform", "rotate(90)")
 		.style("text-anchor", "start");
+
+	barchart
 
 		
 	barchart.append("text")
@@ -599,7 +677,7 @@ function ready([topo, flows, stack, graph, choropleth]) {
 				}
 			}
     };	
-			
+
 	var units = "%";
 	var positionInfo = document.getElementById('sankeyDiv').getBoundingClientRect();
 	var margin = {top: 10, right: 200, bottom: 30, left: 150};
@@ -610,7 +688,7 @@ function ready([topo, flows, stack, graph, choropleth]) {
 	// format variables
 	var formatNumber = d3.format(",.0f"),    // zero decimal places
 		format = function(d) { return formatNumber(d) + " " + units; };
-	
+
 	sankeySvg = sankeyDiv.append("svg")
 		.attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
 		// .attr("width", width + margin.left + margin.right)
